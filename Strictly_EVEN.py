d1f385d75afbcfd8fe50bf39324d72c57eb3024d
bhuvanshashank04 <113907319+bhuvanshashank04@users.noreply.github.com>
2022-12-13 20:06:09 +0530

    t=int(input()) a=list(map(int,input().split())) cnt=cnt1=0 for i in range(t):     if a[i]==0:         cnt+=1         if i%2==0 or i==0:             cnt1+=1     elif a[i]%2==0:         cnt+=1         if i%2==0:             cnt1+=1 if cnt==cnt1:     print(True) else:     print(False)

---
+++ Strictly_EVEN.py
@@ -0,0 +1,16 @@
+t=int(input())
+a=list(map(int,input().split()))
+cnt=cnt1=0
+for i in range(t):
+    if a[i]==0:
+        cnt+=1
+        if i%2==0 or i==0:
+            cnt1+=1
+    elif a[i]%2==0:
+        cnt+=1
+        if i%2==0:
+            cnt1+=1
+if cnt==cnt1:
+    print(True)
+else:
+    print(False)
