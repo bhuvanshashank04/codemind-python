483bd9048f4f1fa0885fe934e1edaf84c639fc46
bhuvanshashank04 <113907319+bhuvanshashank04@users.noreply.github.com>
2022-12-25 13:35:54 +0530

    t=int(input()) a=list(map(int,input().split())) cnt=0 for i in range(1,t):     if a[i-1]<a[i]:         pass     else:         cnt=1         break if cnt==0:     print('yes') else:     print('no')

---
+++ Ascending_array.py
@@ -0,0 +1,13 @@
+t=int(input())
+a=list(map(int,input().split()))
+cnt=0
+for i in range(1,t):
+    if a[i-1]<a[i]:
+        pass
+    else:
+        cnt=1
+        break
+if cnt==0:
+    print('yes')
+else:
+    print('no')
