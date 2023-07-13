3cf44e037ca153957d626527dcbaf0fc346e483d
bhuvanshashank04 <113907319+bhuvanshashank04@users.noreply.github.com>
2023-07-13 10:16:27 +0530

    a=int(input()) b=list(map(int,input().split())) c=[] d=[] e=[] for i in b:     if i%2==0:         c.append(i)     else:         d.append(i) f=len(c) g=len(d) h=min(f,g) j=max(f,g)-h for i in range(h):     e.append(c[i])     e.append(d[i]) for i in range(j):     if(f>g):         e.append(c[h+i])     else:         e.append(d[h+i]) if(len(e)%2!=0):     e.append(0) print(*e)

---
+++ even_odd_pairs.py
@@ -0,0 +1,25 @@
+a=int(input())
+b=list(map(int,input().split()))
+c=[]
+d=[]
+e=[]
+for i in b:
+    if i%2==0:
+        c.append(i)
+    else:
+        d.append(i)
+f=len(c)
+g=len(d)
+h=min(f,g)
+j=max(f,g)-h
+for i in range(h):
+    e.append(c[i])
+    e.append(d[i])
+for i in range(j):
+    if(f>g):
+        e.append(c[h+i])
+    else:
+        e.append(d[h+i])
+if(len(e)%2!=0):
+    e.append(0)
+print(*e)
